7c05629156f106f883ccf1d6ff8366b9fd1e762f
Tushar Mann <tusharmann29@gmail.com>
2017-03-15 15:45:08 +0530

    all errors resolved

--- app/src/main/java/com/example/tushar/smartmuseum/MainActivity.java
+++ app/src/main/java/com/example/tushar/smartmuseum/MainActivity.java
@@ -7,11 +7,15 @@ import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         if (!havePermissions()) {
@@ -109,9 +114,9 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         Snackbar.make(mContainer,
-                R.string.permission_denied_explanation,
-                Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.settings, new View.OnClickListener() {
+                " Permission was denied, but is needed. Go to Settings.",
+                Snackbar.LENGTH_LONG)
+                .setAction("Settings", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         // Build intent that displays the App settings screen.
@@ -131,13 +136,13 @@ public class MainActivity extends AppCompatActivity {
         if (mContainer == null) {
             return;
         }
-        Snackbar.make(mContainer, R.string.permission_rationale,
-                Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.ok, new View.OnClickListener() {
+        Snackbar.make(mContainer, "Location permission is required when scanning for beacons with Nearby.",
+                Snackbar.LENGTH_LONG)
+                .setAction("OK", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         // Request permission.
-                        ActivityCompat.requestPermissions(ShoppingCartActivity.this,
+                        ActivityCompat.requestPermissions(MainActivity.this,
                                 mPermissions,
                                 PERMISSIONS_REQUEST_CODE);
                     }
@@ -157,6 +162,61 @@ public class MainActivity extends AppCompatActivity {
         };
         MyApp.getInstance().onListRefreshListener = onListRefreshListener;
         MyApp.getInstance().context = this;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != PERMISSIONS_REQUEST_CODE) {
+            return;
+        }
+        for (int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,permission)) {
+                    Log.i(TAG, "Permission denied without 'NEVER ASK AGAIN': " + permission);
+                    showRequestPermissionsSnackbar();
+                } else {
+                    Log.i(TAG, "Permission denied with 'NEVER ASK AGAIN': " + permission);
+                    showLinkToSettingsSnackbar();
+                }
+            } else {
+                Log.i(TAG, "Permission granted, building GoogleApiClient");
+            }
+        }
+    }
+
+    private void notifyListChange(){
+        if(arrayAdapter != null){
+            List<String> items = new ArrayList<>(MyApp.getInstance().regionNameList);
+            arrayAdapter.clear();
+            arrayAdapter.addAll(items);
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            //logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        MyApp.getInstance().onListRefreshListener = null;
+        MyApp.getInstance().context = null;
     }
 
 }
